fae2331e5dd0d2d9d63bee53397c8b0d277d88e8
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-11-24 10:56:09 -0300

    Adding a advise message

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:welcome/widgets/profile_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,12 +8,16 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: ProfileWidget(),
+          const ProfileWidget(),
+          const Divider(height: 50),
+          Text(
+            AppLocalizations.of(context)!.adviseMessage,
+            style: Theme.of(context).textTheme.titleLarge,
           ),
         ],
       ),
