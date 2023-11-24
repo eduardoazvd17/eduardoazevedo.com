caae8fb5982d9d5ffcb027fc4357b392621f5b30
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-11-24 11:22:42 -0300

    Adjusting for mobile

--- lib/widgets/profile_widget.dart
+++ lib/widgets/profile_widget.dart
@@ -8,15 +8,32 @@ class ProfileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        _profilePicture(context),
-        const SizedBox(width: 15),
-        _profileData(context),
-      ],
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth >= 768) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profilePicture(context),
+            const SizedBox(width: 15),
+            Flexible(child: _profileData(context)),
+          ],
+        );
+      } else {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profilePicture(context),
+            const SizedBox(height: 15),
+            _profileData(
+              context,
+              crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+          ],
+        );
+      }
+    });
   }
 
   Widget _profilePicture(BuildContext context) {
@@ -36,63 +53,70 @@ class ProfileWidget extends StatelessWidget {
     );
   }
 
-  Widget _profileData(BuildContext context) {
-    return Flexible(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Eduardo Azevedo Regueira',
-                style: Theme.of(context).textTheme.titleLarge,
+  Widget _profileData(
+    BuildContext context, {
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.start,
+  }) {
+    final TextAlign textAlign = crossAxisAlignment == CrossAxisAlignment.center
+        ? TextAlign.center
+        : TextAlign.start;
+
+    return Column(
+      crossAxisAlignment: crossAxisAlignment,
+      children: [
+        Column(
+          crossAxisAlignment: crossAxisAlignment,
+          children: [
+            Text(
+              'Eduardo Azevedo Regueira',
+              textAlign: textAlign,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5.0),
+              child: Text(
+                AppLocalizations.of(context)!.profileTitle,
+                textAlign: textAlign,
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5.0),
-                child: Text(
-                  AppLocalizations.of(context)!.profileTitle,
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
+            ),
+          ],
+        ).animate().fade().slideX(),
+        Wrap(
+          spacing: 15,
+          runSpacing: 15,
+          children: [
+            InkWell(
+              onTap: () => _open(
+                'https://www.linkedin.com/in/eduardoazvd17/',
               ),
-            ],
-          ).animate().fade().slideX(),
-          Wrap(
-            spacing: 15,
-            runSpacing: 15,
-            children: [
-              InkWell(
-                onTap: () => _open(
-                  'https://www.linkedin.com/in/eduardoazvd17/',
-                ),
-                borderRadius: BorderRadius.circular(5),
-                child: Padding(
-                  padding: const EdgeInsets.all(2),
-                  child: Image.network(
-                    'assets/icons/linkedin.png',
-                    color: Colors.blueAccent,
-                    width: 90,
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () => _open(
-                  'https://github.com/eduardoazvd17/',
-                ),
-                borderRadius: BorderRadius.circular(5),
-                child: Padding(
-                  padding: const EdgeInsets.all(2),
-                  child: Image.network(
-                    'assets/icons/github.png',
-                    width: 90,
-                    color: Colors.deepPurple,
-                  ),
+              borderRadius: BorderRadius.circular(5),
+              child: Padding(
+                padding: const EdgeInsets.all(2),
+                child: Image.network(
+                  'assets/icons/linkedin.png',
+                  color: Colors.blueAccent,
+                  width: 90,
                 ),
               ),
-            ],
-          ).animate().fade().slideY(),
-        ],
-      ),
+            ),
+            InkWell(
+              onTap: () => _open(
+                'https://github.com/eduardoazvd17/',
+              ),
+              borderRadius: BorderRadius.circular(5),
+              child: Padding(
+                padding: const EdgeInsets.all(2),
+                child: Image.network(
+                  'assets/icons/github.png',
+                  width: 90,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ),
+          ],
+        ).animate().fade().slideY(),
+      ],
     );
   }
 
