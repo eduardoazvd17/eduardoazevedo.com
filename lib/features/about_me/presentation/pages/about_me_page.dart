431055e6d2df7fc4c96bc4acee19f697f7a2655f
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-11-29 18:12:32 -0300

    refactor: modularizing application

---
+++ lib/features/about_me/presentation/pages/about_me_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AboutMePage extends StatelessWidget {
+  static const String route = '/aboutMe';
+  const AboutMePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('About me page content...'),
+      ],
+    );
+  }
+}
---
+++ lib/features/my_projects/presentation/pages/my_projects_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyProjectsPage extends StatelessWidget {
+  static const String route = '/myProjects';
+  const MyProjectsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('My projects page content...'),
+      ],
+    );
+  }
+}
