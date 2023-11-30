18f45f8274dfd0ec9579149aee14c5af06a1e029
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-11-30 18:24:05 -0300

    Fixing private field

--- lib/src/core/presentation/controllers/app_controller.dart
+++ lib/src/core/presentation/controllers/app_controller.dart
@@ -1,6 +1,6 @@
 import 'package:mobx/mobx.dart';
 part 'app_controller.g.dart';
 
-class AppController = _AppControllerBase with _$AppController;
+class AppController = AppControllerBase with _$AppController;
 
-abstract class _AppControllerBase with Store {}
+abstract class AppControllerBase with Store {}
