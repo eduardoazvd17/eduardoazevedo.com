4d760e90494c560d44cb07e3db3bce7171b2f644
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-11-30 11:34:16 -0300

    Creating app themes files

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,13 @@ import 'package:eduardoazevedo/src/features/home/presentation/controllers/home_c
 import 'package:flutter/material.dart';
 
 import 'src/core/utils/app_constants.dart';
+import 'src/core/utils/app_themes.dart';
 import 'src/features/home/presentation/pages/home_page.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  ThemeData get lightThemeData => ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          brightness: Brightness.light,
-          surfaceTint: Colors.white,
-        ),
-        primaryColor: Colors.blue,
-        scaffoldBackgroundColor: Colors.blueGrey[50],
-        useMaterial3: true,
-      );
-
-  ThemeData get darkThemeData => ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          brightness: Brightness.dark,
-        ),
-        primaryColor: Colors.blue,
-        scaffoldBackgroundColor: Colors.black,
-        shadowColor: Colors.grey[700],
-        cardTheme: CardTheme(
-          shadowColor: Colors.grey[700],
-        ),
-        useMaterial3: true,
-      );
 
   @override
   Widget build(BuildContext context) {
@@ -41,8 +17,8 @@ class MyApp extends StatelessWidget {
       title: 'Eduardo Azevedo',
       localizationsDelegates: AppConstants.localizationsDelegates,
       supportedLocales: AppConstants.supportedLocales,
-      theme: lightThemeData,
-      darkTheme: darkThemeData,
+      theme: AppThemes.light,
+      darkTheme: AppThemes.dark,
       initialRoute: HomePage.route,
       routes: {
         HomePage.route: (_) => HomePage(controller: HomeController()),
