f9426d891de08574db5c3b7f687e51d5e77b2168
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-11-30 14:34:21 -0300

    Adding new navigation bar

--- lib/src/features/home/presentation/pages/home_page.dart
+++ lib/src/features/home/presentation/pages/home_page.dart
@@ -4,8 +4,8 @@ import 'package:eduardoazevedo/src/features/home/data/enums/home_page_tabs.dart'
 import 'package:eduardoazevedo/src/features/home/presentation/controllers/home_controller.dart';
 import 'package:eduardoazevedo/src/features/my_projects/presentation/pages/my_projects_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 import '../../../../core/widgets/responsive_builder.dart';
 import '../widgets/profile_widget.dart';
@@ -29,15 +29,25 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           const Padding(
-            padding: EdgeInsets.symmetric(vertical: 58),
+            padding: EdgeInsets.symmetric(vertical: 60),
             child: ProfileWidget(),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 58),
-            child: _navigationBar(context),
+            padding: const EdgeInsets.symmetric(horizontal: 60),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Divider(height: 0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: _navigationBar(context),
+                ),
+                const Divider(height: 0),
+              ],
+            ),
           ),
           Padding(
-            padding: const EdgeInsets.all(58),
+            padding: const EdgeInsets.all(60),
             child: _pageContent(context),
           ),
         ],
@@ -47,12 +57,18 @@ class HomePage extends StatelessWidget {
 
   Widget _mobileScaffold(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: _bottomNavigationBar(context),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Divider(height: 0),
+          _navigationBar(context),
+        ],
+      ),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 25),
         children: [
           const Padding(
-            padding: EdgeInsets.symmetric(vertical: 58),
+            padding: EdgeInsets.symmetric(vertical: 60),
             child: ProfileWidget(),
           ),
           Padding(
@@ -64,95 +80,28 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _bottomNavigationBar(BuildContext context) => Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Divider(height: 0),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            child: Observer(
-              builder: (context) {
-                return BottomNavigationBar(
-                  currentIndex: controller.currentPage.index,
-                  onTap: (index) => controller.changePage(
-                    HomePageTabs.values[index],
-                  ),
-                  items: HomePageTabs.values.map((e) {
-                    return BottomNavigationBarItem(
-                      icon: Icon(e.icon),
-                      label: e.title(context),
-                    );
-                  }).toList(),
-                ).animate().fade();
-              },
+  Widget _navigationBar(BuildContext context) => Observer(
+        builder: (context) {
+          return GNav(
+            selectedIndex: controller.currentPage.index,
+            onTabChange: (index) => controller.changePage(
+              HomePageTabs.values[index],
             ),
-          ),
-        ],
+            tabBorderRadius: 16,
+            color: Theme.of(context).iconTheme.color,
+            activeColor: Theme.of(context).primaryColor,
+            mainAxisAlignment: MainAxisAlignment.center,
+            tabs: HomePageTabs.values.map((e) {
+              return GButton(
+                gap: 10,
+                icon: e.icon,
+                text: e.title(context),
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+              );
+            }).toList(),
+          );
+        },
       );
-
-  Widget _navigationBar(BuildContext context) => Column(
-        children: [
-          const Divider(height: 0),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25),
-            child: Observer(
-              builder: (context) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: HomePageTabs.values.map((page) {
-                    final bool isSelected = controller.currentPage == page;
-
-                    return DecoratedBox(
-                      decoration: BoxDecoration(
-                        border: isSelected
-                            ? BorderDirectional(
-                                bottom: BorderSide(
-                                  width: 1.5,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              )
-                            : null,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 5.0),
-                        child: InkWell(
-                          onTap: () => controller.changePage(page),
-                          borderRadius: BorderRadius.circular(5),
-                          child: Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(
-                                  page.icon,
-                                  color: isSelected
-                                      ? Theme.of(context).primaryColor
-                                      : null,
-                                ),
-                                const SizedBox(width: 10),
-                                Text(
-                                  page.title(context),
-                                  style: isSelected
-                                      ? TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Theme.of(context).primaryColor,
-                                        )
-                                      : null,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                );
-              },
-            ),
-          ),
-          const Divider(height: 0),
-        ],
-      ).animate().fade();
 
   Widget _pageContent(context) {
     return Observer(
