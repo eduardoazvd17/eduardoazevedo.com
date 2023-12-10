7ad3fc7bdcbadfdf3a0df8922a3dadfbfb27f286
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-12-10 11:08:55 -0300

    Changing text field

--- lib/src/features/home/presentation/pages/home_page_tabs/contact_tab.dart
+++ lib/src/features/home/presentation/pages/home_page_tabs/contact_tab.dart
@@ -115,21 +115,13 @@ class ContactTab extends StatelessWidget {
   }) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: AppThemes.commonColor),
-          borderRadius: AppThemes.circular5,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 10, left: 10, right: 10),
-          child: TextField(
-            maxLines: isTextArea ? 5 : 1,
-            controller: controller,
-            decoration: InputDecoration(
-              prefixIcon: icon != null ? Icon(icon) : null,
-              label: Text(label),
-            ),
-          ),
+      child: TextFormField(
+        controller: controller,
+        maxLines: isTextArea ? 5 : 1,
+        decoration: InputDecoration(
+          prefixIcon: icon != null ? Icon(icon) : null,
+          label: Text(label),
+          border: const OutlineInputBorder(),
         ),
       ),
     );
