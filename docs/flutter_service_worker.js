9dcef7cb9d47dfc58790d3d7181a47d3cffb66df
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-12-12 11:45:02 -0300

    New deploy to github pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"version.json": "39d88416924031e2c98a0fae15e055e7",
-"index.html": "b773e04fdbc719700e97db6a151c61b0",
-"/": "b773e04fdbc719700e97db6a151c61b0",
-"main.dart.js": "996c5b21a47610fea7cf84d2ad1931b1",
+const RESOURCES = {"version.json": "03ef01827debaac941443771f34476f8",
+"index.html": "36b05a623ff32e3ae00266103a9de6be",
+"/": "36b05a623ff32e3ae00266103a9de6be",
+"main.dart.js": "7e06f5e6b209df8003e706620d8a291a",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "7e6663123a9ea804e75a340ff4c0f1c1",
 "icons/Icon-192.png": "7e6663123a9ea804e75a340ff4c0f1c1",
