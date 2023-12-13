329c145751cd5abfc522cf548705d54acc1f0885
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-12-12 22:17:18 -0300

    Adding languages to about page

--- lib/src/features/home/presentation/pages/home_page_tabs/about_me_tab.dart
+++ lib/src/features/home/presentation/pages/home_page_tabs/about_me_tab.dart
@@ -24,6 +24,44 @@ class AboutMeTab extends StatelessWidget {
         Text(AppLocalizations.of(context)!.apresentationText3),
         const SizedBox(height: 10),
         Text(AppLocalizations.of(context)!.apresentationText4),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40),
+          child: _languagesIconsWidget(),
+        ),
+      ],
+    );
+  }
+
+  Widget _languagesIconsWidget() {
+    const icons = [
+      'flutter.png',
+      'csharp.png',
+      'java.png',
+      'javascript.png',
+      'python.png'
+    ];
+
+    return Row(
+      children: [
+        Expanded(
+          child: Wrap(
+            spacing: 15,
+            runSpacing: 15,
+            alignment: WrapAlignment.spaceEvenly,
+            runAlignment: WrapAlignment.spaceEvenly,
+            children: icons.map((icon) {
+              final String name = icon.replaceAll('.png', '');
+              return Tooltip(
+                message: name,
+                child: Image.asset(
+                  'assets/icons/$icon',
+                  height: 50,
+                  semanticLabel: name,
+                ),
+              );
+            }).toList(),
+          ),
+        ),
       ],
     );
   }
